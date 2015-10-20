eeea89c81c612bde66f13cb7d116f3fd30dffb12
Steve Lombardi <smlombardi@gmail.com>
2015-10-20 10:58:20 -0400

    fixed nav size for desktop and ipad
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- template-part-head.php
+++ template-part-head.php
@@ -9,7 +9,7 @@
 
     <div class="logo-left">
       <a href="<?php echo get_settings('home'); ?>">
-      <img src="<?php bloginfo('template_directory'); ?>/img/logo-left.png" alt="New Preston Kitchen Goods">
+      <img src="<?php bloginfo('template_directory'); ?>/img/logo-left.png" alt="New Preston Kitchen Goods" class="img-responsive">
     </a>
     </div>
 
