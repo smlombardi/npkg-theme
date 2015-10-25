aa28336f4b1b4c015fecf5eff16928b3f90c9a7d
Steve Lombardi <smlombardi@gmail.com>
2015-10-25 11:15:26 -0400

    re-did products template
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- page-productgroup.php
+++ page-productgroup.php
@@ -43,10 +43,10 @@
         </div>
 
         <div class="hero-image">
-            <?php
+            <!-- <?php
             if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
               the_post_thumbnail('full');
-            } ?>
+            } ?> -->
         </div>
 
         </div>
