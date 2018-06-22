07915b389b2c9a142b884f1c87b97425f6e34709
Steve Lombardi <smlombardi@gmail.com>
2018-06-22 18:13:09 -0400

    update logos to retina

--- template-part-head-home.php
+++ template-part-head-home.php
@@ -1,15 +1,9 @@
 <?php global $dm_settings; ?>
-
-
-
-
     <div class="container dmbs-container">
       <div class="row">
         <div class="col-md-12">
           <div class="checker-header">
-            <!-- <img src="<?php bloginfo('template_directory'); ?>/img/logo670.png" alt="New Preston Kitchen Goods" class="img-responsive"> -->
             <img src="<?php bloginfo('template_directory'); ?>/img/npkg-logo.png" srcset="<?php bloginfo('template_directory'); ?>/img/npkg-logo.png 1x, <?php bloginfo('template_directory'); ?>/img/npkg-logo@2x.png 2x" class="img-responsive"  alt="New Preston Kitchen Goods" >
-   
           </div>
         </div>
       </div>
--- template-part-head.php
+++ template-part-head.php
@@ -9,7 +9,8 @@
 
     <div class="logo-left">
       <a href="<?php echo get_settings('home'); ?>">
-      <img src="<?php bloginfo('template_directory'); ?>/img/logo-left.png" alt="New Preston Kitchen Goods" class="img-responsive">
+    
+      <img src="<?php bloginfo('template_directory'); ?>/img/logo-left.png" srcset="<?php bloginfo('template_directory'); ?>/img/logo-left.png 1x, <?php bloginfo('template_directory'); ?>/img/logo-left@2x.png 2x" class="img-responsive" alt="New Preston Kitchen Goods">
     </a>
     </div>
 
