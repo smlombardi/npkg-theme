2b53b07b08827988289c6f158e46f92dea7172ee
Steve Lombardi <smlombardi@gmail.com>
2018-06-23 10:00:43 -0400

    new images

--- template-part-head-products.php
+++ template-part-head-products.php
@@ -8,14 +8,16 @@
 <header>
 
     <div class="logo-left">
-      <a href="<?php echo get_settings('home'); ?>">
-      <img src="<?php bloginfo('template_directory'); ?>/img/logo-left.png" alt="New Preston Kitchen Goods" class="img-responsive">
+      <a href="<?php echo get_settings('home'); ?>">  
+      <img src="<?php bloginfo('template_directory'); ?>/img/logo-left.png" srcset="<?php bloginfo('template_directory'); ?>/img/logo-left.png 1x, <?php bloginfo('template_directory'); ?>/img/logo-left@2x.png 2x" class="img-responsive" alt="New Preston Kitchen Goods">
     </a>
+
     </div>
 
 
     <div class="section-box">
-      <img src="<?php bloginfo('template_directory'); ?>/img/box-products.png" alt="" class="img-responsive">
+     <?php $img = get_template_directory_uri() . '/img/box-products' ?>
+      <img src="<?php echo $img; ?>.png" srcset="<?php echo $img; ?>.png 1x, <?php echo $img; ?>@2x.png 2x" class="img-responsive">
     </div>
 
 </header>
